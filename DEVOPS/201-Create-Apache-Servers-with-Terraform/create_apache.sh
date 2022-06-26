c2cdcb52459a3cf237652d273a7d7f72c8cad0e2
sersah95 <sersah95@gmail.com>
2022-06-27 02:57:58 +0300

    devops create apache servers with terraform

--- DEVOPS/201-Create-Apache-Servers-with-Terraform/create_apache.sh
+++ DEVOPS/201-Create-Apache-Servers-with-Terraform/create_apache.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo su
 yum -y install httpd
-echo "<p> Hello World </p>" >> /var/www/html/index.html
+echo "<p> Hello World- Wellcome to my first DevOps Projects </p>" >> /var/www/html/index.html
 sudo systemctl enable httpd
 sudo systemctl start httpd
