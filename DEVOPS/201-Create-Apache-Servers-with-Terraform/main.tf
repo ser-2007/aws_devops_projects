01d8fcfb7fc919b06a0a2726801a46dc2956a01a
sersah95 <sersah95@gmail.com>
2022-06-27 03:07:34 +0300

    main tf added

--- DEVOPS/201-Create-Apache-Servers-with-Terraform/main.tf
+++ DEVOPS/201-Create-Apache-Servers-with-Terraform/main.tf
@@ -1,20 +1,55 @@
+# Adam's solution
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "3.42.0"
+      source  = "hashicorp/aws"
+      version = "3.58.0"
     }
   }
 }
-
 provider "aws" {
-  region  = "us-east-1"
-#   profile = "cw-training"
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "allow-ssh" {
+  name        = "allow-ssh"
+  description = "Port 22, 80 and 443 from all world"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    description = "TLS from VPC"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    description = "worldwide"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
+    description = "worldwide"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 data "aws_ami" "amazon-linux-2" {
-  most_recent = true
   owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
   filter {
     name   = "owner-alias"
     values = ["amazon"]
@@ -23,67 +58,28 @@ data "aws_ami" "amazon-linux-2" {
     name   = "name"
     values = ["amzn2-ami-hvm*"]
   }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
 }
-
-resource "aws_instance" "apache-server" {
-  ami             = data.aws_ami.amazon-linux-2.id
-  instance_type   = "t2.micro"
-//  key_name        = "northvirginia"
-  count           = 2
-  security_groups = ["tf-project"]
-  user_data = file("create_apache.sh")
-
+resource "aws_instance" "apache-ec2" {
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = "t2.micro"
+  count                  = 2
+  key_name               = "ec2_key"
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+  user_data              = file("create_apache.sh")
   tags = {
-    Name = "terraform ${element(var.mytags, count.index)} instance"
+    Name = "Terraform ${element(var.mytags, count.index)} Instance"
   }
-    provisioner "local-exec" {
-      command = "echo ${self.private_ip} >> private_ip.txt"
-    }
-    provisioner "local-exec" {
-      command = "echo ${self.public_ip} >> public_ip.txt"
-    }
-}
-
-resource "aws_security_group" "tf-sec-gr" {
-  name = "tf-project"
-
-  ingress {
-    from_port   = 443
-    protocol    = "tcp"
-    to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
+  provisioner "local-exec" {
+    command = "echo ${self.private_ip} >> private_ip.txt"
   }
-
-  ingress {
-    from_port   = 80
-    protocol    = "tcp"
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    protocol    = -1
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> public_ip.txt"
   }
 }
 variable "mytags" {
   type    = list(string)
-  default = ["first", "second"]
+  default = ["First", "Second"]
 }
-
-output "mypublicip" {
-  value = aws_instance.apache-server[*].public_ip
+output "my-public-ip" {
+  value = aws_instance.apache-ec2[*].public_ip
 }
