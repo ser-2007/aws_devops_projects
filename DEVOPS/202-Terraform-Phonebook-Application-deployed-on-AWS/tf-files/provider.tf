8ef46d31d961ffa2018b903ac78a8b22c67cdb51
sersah95 <sersah95@gmail.com>
2022-06-29 16:09:42 +0300

    provider tf updated

--- DEVOPS/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/provider.tf
+++ DEVOPS/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/provider.tf
@@ -19,5 +19,5 @@ provider "aws" {
 
 provider "github" {
   # Configuration options
-  token = "xxxxxxxxxxxxxxxxxxxxxxxxx"
+  token = "xxxxxxxxxxxxxxxxxxxxxxxxx" #(your git hub token must be written)
 }
