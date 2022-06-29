78e395bfed6b98b301d482c3917b231b0e563134
sersah95 <sersah95@gmail.com>
2022-06-29 15:59:35 +0300

    user data sh added

---
+++ DEVOPS/202-Terraform-Phonebook-Application-deployed-on-AWS/user-data.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+yum update -y
+yum install python3 -y
+pip3 install flask
+pip3 install flask_mysql
+yum install git -y
+TOKEN="xxxxxxxxxxxxxxx"
+cd /home/ec2-user && git clone https://$TOKEN@github.com/ser-2007/aws_devops_projects/tree/main/DEVOPS/202-Terraform-Phonebook-Application-deployed-on-AWS.git
+python3 /home/ec2-user/phonebook/phonebook-app.py
