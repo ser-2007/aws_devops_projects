d3d07dd905cb73eb07a17cedd7eb7cc492718681
sersah95 <sersah95@gmail.com>
2022-06-30 16:46:15 +0300

    userdata updatd

--- DEVOPS/202-Terraform-Phonebook-Application-deployed-on-AWS/user-data.sh
+++ DEVOPS/202-Terraform-Phonebook-Application-deployed-on-AWS/user-data.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="xxxxxxxxxxxxxxx"
+TOKEN="xxxxxxxxxxxxxxx" #write your own token for thi project
 cd /home/ec2-user && git clone https://$TOKEN@github.com/ser-2007/aws_devops_projects/tree/main/DEVOPS/202-Terraform-Phonebook-Application-deployed-on-AWS.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
