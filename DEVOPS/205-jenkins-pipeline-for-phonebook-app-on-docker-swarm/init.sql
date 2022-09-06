08dac309763da8113cfb5a223a714114718c8fcd
sersah95 <sersah95@gmail.com>
2022-09-06 23:58:12 +0300

    sql init file added

---
+++ DEVOPS/205-jenkins-pipeline-for-phonebook-app-on-docker-swarm/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE phonebook_db.phonebook(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    number VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO phonebook_db.phonebook (name, number)
+    VALUES
+        ("Callahan", "1234567890"),
+        ("Serkan", "6537798080"),
+        ("Sergio Taco", "67854"),
+        ("Vincenzo Altobelli", "876543554");
