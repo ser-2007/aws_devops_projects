cb01f9b7e824e0e338f831b4267f2086f3a8d86c
sersah95 <sersah95@gmail.com>
2022-11-17 03:24:26 +0200

    sec group add

---
+++ DEVOPS/kubernetes_projects_handsons/k3s/k3s-with-terraform/sec-group-stable.tf
@@ -0,0 +1,103 @@
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default-VPC"
+  }
+}
+
+
+
+resource "aws_security_group" "k3s_server" {
+  name = "k3s-server"
+  #description = "Allow TLS inbound traffic"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress = [
+    {
+      description      = "for ssh"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+      #security_group_id = aws_security_group.k3s_server.id      
+    },
+  
+    {
+      description      = "for k8s"
+      from_port        = 6443
+      to_port          = 6443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+      #security_group_id = aws_security_group.k3s_server.id      
+    },
+    
+
+  ]
+  egress = [
+    {
+      description      = "allow all"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+    }
+  ]
+
+  tags = {
+    Name = "k3s-server-sg"
+  }
+}
+
+
+resource "aws_security_group" "k3s_agent" {
+  name = "k3s-agent"
+  #description = "Allow TLS inbound traffic"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress = [
+    {
+      description      = "for ssh"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+      #security_group_id = aws_security_group.k3s_server.id      
+    }
+    
+  ]
+
+  egress = [
+    {
+      description      = null
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups  = null
+      self             = null
+
+    }
+  ]
+
+  tags = {
+    Name = "k3s-agent-sg"
+  }
+}
+
